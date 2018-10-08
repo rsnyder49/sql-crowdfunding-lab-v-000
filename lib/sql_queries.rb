4c6406b8ac5eec6ed47e693ffe53372796be43c0
Robert Snyder <r.snyder49@gmail.com>
2018-10-08 19:19:50 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -14,7 +14,7 @@ GROUP BY(projects.title);"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT users.name, users.age SUM(pledges.amount)
+"SELECT users.name, users.age, SUM(pledges.amount)
 FROM users
 JOIN pledges ON users.id == user_id
 GROUP BY(users.name);"
