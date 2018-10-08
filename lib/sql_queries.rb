5e0c5aea6fc9d7d1977b9781828948647d447568
Robert Snyder <r.snyder49@gmail.com>
2018-10-08 19:25:32 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -32,6 +32,7 @@ end
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
 "SELECT users.name, SUM(pledges.amount) 
 FROM users 
+JOIN pledges 
 "
 end
 
