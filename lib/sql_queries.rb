91a06751e5b6d86fbe2faf957033e777c1e1f81b
Robert Snyder <r.snyder49@gmail.com>
2018-10-08 19:29:38 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,7 +35,7 @@ FROM users
 JOIN pledges 
 ON users.id == pledges.user_id 
 GROUP BY users.id 
-ORDER BY SUM(;"
+ORDER BY SUM(pledges.amount);"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
