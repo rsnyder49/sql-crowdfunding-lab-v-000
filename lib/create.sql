2570d2ab9ddd7a5b1c9656f70e72a018b2eb6747
Robert Snyder <r.snyder49@gmail.com>
2018-10-08 18:54:25 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -5,17 +5,17 @@ CREATE TABLE projects (
   funding_goal INTEGER, 
   start_date TEXT,
   end_date TEXT
-)
+);
 
 CREATE TABLE users (
   id INTEGER PRIMARY KEY, 
   name TEXT,
   age INTEGER
-)
+);
 
 CREATE TABLE pledge (
   id INTEGER PRIMARY KEY,
   amount INTEGER,
   user_id INTEGER,
   project_id INTEGER
-)
+);
