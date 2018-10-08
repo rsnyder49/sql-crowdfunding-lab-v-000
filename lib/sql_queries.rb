90d676b21fdb1e998fde1642d09118021a947e74
Robert Snyder <r.snyder49@gmail.com>
2018-10-08 19:25:16 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -30,7 +30,8 @@ ORDER BY projects.title;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, "
+"SELECT users.name, SUM(pledges.amount) 
+"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
